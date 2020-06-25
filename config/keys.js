75275e3b9afc53e70763bb700f6252f1b719c0d1
danlopes-arc <danlpsarcanjo@gmail.com>
2020-06-25 19:48:20 -0400

    Change keys.js and add it to .gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb+srv://mern-shopping-list:fKhMllJU6kyxMrcZ@cluster0-mstg6.mongodb.net/mern-shopping-list?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://mern-shopping-list:EPwGyOFpsTHgGwp2@cluster0-mstg6.mongodb.net/mern-shopping-list?retryWrites=true&w=majority"
 }
